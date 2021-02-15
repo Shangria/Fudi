7a320efe15dc97010c08fce4a5e92b93897d00a7
shangria <shangrialena@gmail.ru>
2021-02-16 00:46:43 +0200

    header & banner

--- src/js/index.js
+++ src/js/index.js
@@ -2,11 +2,7 @@ import '../scss/style.scss'
 
 // import './scripts/data-storage.js';
 //
-// import './scripts/header-menu.js';
 //
-// import './scripts/header-scroll.js';
-//
-// import './scripts/header-sub-menu.js';
 //
 // import './scripts/timer.js';
 //
